4125de91951b1543548fa312f21f4d2c2cc98f14
seungin <tmddls91@inha.edu>
2025-07-08 00:38:46 +0900

    [refactor] TodoWriteForm 입력값에 대해 유효성 검사 및 공백 제거 추가 (#1)

--- src/components/TodoWriteForm.tsx
+++ src/components/TodoWriteForm.tsx
@@ -10,8 +10,13 @@ function TodoWriteForm() {
 
         const form = e.currentTarget
         const input = form.todo as HTMLInputElement
+        const value = input.value.trim() // 앞 뒤 공백 제거
 
-        addTodo(input.value)
+        if (!value) {
+            return
+        } // 입력값이 비어있으면 아무 작업도 하지 않음
+        addTodo(value) // 입력값이 비어있지 않으면 addTodo 호출
+        input.value = '' // 입력 후 input 초기화
     }
 
     return (
