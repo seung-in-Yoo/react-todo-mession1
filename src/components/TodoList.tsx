a4ce9ea98a719f7606c2bf2649aaf32b57f2af6d
seungin <tmddls91@inha.edu>
2025-07-08 00:23:24 +0900

    [feat] 할일 리스트를 나타내는 TodoList 컴포넌트 구현 (#1)

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,16 @@
+import { useTodos } from '../context/TodoContext'
+import TodoItem from './TodoItem'
+
+function TodoList() {
+    const { todos } = useTodos()
+
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <TodoItem key={todo.id} todo={todo} />
+            ))}
+        </ul>
+    )
+}
+
+export default TodoList
